54f9cdc1e15bd02b854ac46a2d1f1f749ec02b99
HussX <greogry.hussey@gmail.com>
2024-07-03 10:48:43 -0400

    Reverted screen on checks.  These failed despite the screen being on.

--- startup.sh
+++ startup.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-if xset q &>/dev/null; then
-	echo "Display is available. Ensuring the screen is on and launching the application..."
+export QT_QPA_PLATFORM=eglfs
+export QT_QPA_EGLFS_SWAPINTERVAL=0
+export QT_QPA_EGLFS_HIDECURSOR=1
 
-	export QT_QPA_PLATFORM=eglfs
-	export QT_QPA_EGLFS_SWAPINTERVAL=0
-	export QT_QPA_EGLFS_HIDECURSOR=1
+python3 /opt/piplay/piplay.py
 
-	python3 /opt/piplay/piplay.py
-else
-	echo "No display detected. Please turn on the screen and try again."
-    exit 1
-fi
