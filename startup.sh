f3b8152e808a10a8a6499caecc3fa96482aeb84d
HussX <greogry.hussey@gmail.com>
2024-07-30 11:19:35 -0400

    #6 Override QT for Pi 5 Corrected and tested backward.

--- startup.sh
+++ startup.sh
@@ -3,5 +3,13 @@ export QT_QPA_PLATFORM=eglfs
 export QT_QPA_EGLFS_SWAPINTERVAL=0
 export QT_QPA_EGLFS_HIDECURSOR=1
 
+systype=$( cat /proc/cpuinfo | grep Model | cut -d ":" -f 2 )
+sub='Pi 5'
+if [[ "$systype" == *"$sub"* ]]
+    then
+        echo '{ "device": "/dev/dri/card1" }' > eglfs.json
+        export QT_QPA_EGLFS_KMS_CONFIG=eglfs.json
+fi
+
 python3 /opt/piplay/piplay.py
 
